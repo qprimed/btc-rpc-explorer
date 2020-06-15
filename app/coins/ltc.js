4b6f0ce54abe41ffce38edc13d1bf99d442ca0bc
qprimed <qprimed@github.com>
2020-06-15 18:07:02 -0400

    inital refactor of ltc.js

--- app/coins/ltc.js
+++ app/coins/ltc.js
@@ -59,46 +59,54 @@ module.exports = {
 	baseCurrencyUnit:currencyUnits[3],
 	defaultCurrencyUnit:currencyUnits[0],
 	feeSatoshiPerByteBucketMaxima: [5, 10, 25, 50, 100, 150, 200, 250],
-	genesisBlockHash: "12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2",
-	genesisCoinbaseTransactionId: "97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9",
-	genesisCoinbaseTransaction: {
-		"txid":"97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9",
-		"hash":"97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9",
-		"blockhash":"12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2",
-		"version":1,
-		"locktime":0,
-		"size":199,
-		"vsize":199,
-		"time":1317972665,
-		"blocktime":1317972665,
-		"vin":[
-			{
-				"prev_out":{
-					"hash":"0000000000000000000000000000000000000000000000000000000000000000",
-					"n":4294967295
-				},
-				"coinbase":"04ffff001d0104404e592054696d65732030352f4f63742f32303131205374657665204a6f62732c204170706c65e280997320566973696f6e6172792c2044696573206174203536"
-			}
-		],
-		"vout":[
-			{
-				"value":"50.00000000",
-				"n":0,
-				"scriptPubKey":{
-					"hex":"040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9 OP_CHECKSIG",
-					"type":"pubkey",
-					"reqSigs":1,
-					"addresses":[
-						"Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2"
-					]
-				}
-			}
-		]
+	genesisBlockHashesByNetwork: {
+		"main": "12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2"
 	},
+	genesisCoinbaseTransactionIdsByNetwork: {
+		"main": "97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"
+	},
+	genesisCoinbaseTransactionsByNetwork: {
+		"main": {
+			"txid":"97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9",
+			"hash":"97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9",
+			"blockhash":"12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2",
+			"version":1,
+			"locktime":0,
+			"size":199,
+			"vsize":199,
+			"time":1317972665,
+			"blocktime":1317972665,
+			"vin":[
+				{
+					"prev_out":{
+						"hash":"0000000000000000000000000000000000000000000000000000000000000000",
+						"n":4294967295
+					},
+					"coinbase":"04ffff001d0104404e592054696d65732030352f4f63742f32303131205374657665204a6f62732c204170706c65e280997320566973696f6e6172792c2044696573206174203536"
+				}
+			],
+			"vout":[
+				{
+					"value":"50.00000000",
+					"n":0,
+					"scriptPubKey":{
+						"hex":"040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9 OP_CHECKSIG",
+						"type":"pubkey",
+						"reqSigs":1,
+						"addresses":[
+							"Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2"
+						]
+					}
+				}
+			]
+		}
+	},
+	genesisCoinbaseOutputAddressScripthash:"a5910e72fee6a2aca123d79e95c5bdd5b4eae54f2f755b57e8df836c099c0db5",
 	historicalData: [
 		{
 			type: "blockheight",
 			date: "2011-10-07",
+			chain: "main",
 			blockHeight: 0,
 			blockHash: "12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2",
 			summary: "The Litecoin genesis block.",
@@ -108,6 +116,7 @@ module.exports = {
 		{
 			type: "tx",
 			date: "2017-05-10",
+			chain: "main",
 			txid: "ce385e55fb2a73fa438426145b074f08314812fa3396472dc572b3079e26e0f9",
 			summary: "First SegWit transaction.",
 			referenceUrl: "https://twitter.com/satoshilite/status/862345830082138113"
@@ -115,6 +124,7 @@ module.exports = {
 		{
 			type: "blockheight",
 			date: "2011-10-13",
+			chain: "main",
 			blockHeight: 448,
 			blockHash: "6995d69ce2cb7768ef27f55e02dd1772d452deb44e1716bb1dd9c29409edf252",
 			summary: "The first block containing a (non-coinbase) transaction.",
@@ -123,6 +133,7 @@ module.exports = {
 		{
 			type: "link",
 			date: "2016-05-02",
+			chain: "main",
 			url: "/rpc-browser?method=verifymessage&args%5B0%5D=Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2&args%5B1%5D=G7W57QZ1jevRhBp7SajpcUgJiGs998R4AdBjcIgJq5BOECh4jHNatZKCFLQeo9PvZLf60ykR32XjT4IrUi9PtCU%3D&args%5B2%5D=I%2C+Charlie+Lee%2C+am+the+creator+of+Litecoin&execute=Execute",
 			summary: "Litecoin's Proof-of-Creator",
 			referenceUrl: "https://medium.com/@SatoshiLite/satoshilite-1e2dad89a017"
@@ -135,7 +146,6 @@ module.exports = {
 			if (responseBody[0] && responseBody[0].price_usd) {
 				return {"usd":responseBody[0].price_usd};
 			}
-			
 			return null;
 		}
 	},
